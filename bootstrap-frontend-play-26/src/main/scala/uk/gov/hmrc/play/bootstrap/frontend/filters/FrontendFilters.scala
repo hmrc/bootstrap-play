9431930f32ab496f8d7e410a21721d26c2e11199
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2021-01-06 13:17:04 +0000

    BDOG-1240 Preserve disabling of filters with deprecated enabled config

--- bootstrap-frontend-play-26/src/main/scala/uk/gov/hmrc/play/bootstrap/frontend/filters/FrontendFilters.scala
+++ bootstrap-frontend-play-26/src/main/scala/uk/gov/hmrc/play/bootstrap/frontend/filters/FrontendFilters.scala
@@ -20,6 +20,9 @@ import javax.inject.{Inject, Singleton}
 import play.api.{Configuration, Logger}
 import play.api.http.{EnabledFilters, HttpFilters}
 import play.api.mvc.EssentialFilter
+import play.filters.csrf.CSRFFilter
+import play.filters.headers.SecurityHeadersFilter
+import scala.reflect.ClassTag
 
 @deprecated("remove config setting play.http.filters = \"uk.gov.hmrc.play.bootstrap.frontend.filters.FrontendFilters\" is no longer required. Bootstrap filters are now configured via frontend.conf", "2.12.0")
 @Singleton
@@ -33,14 +36,33 @@ class FrontendFilters @Inject()(
   private val logger = Logger(getClass)
   logger.warn("play.http.filters = \"uk.gov.hmrc.play.bootstrap.frontend.filters.FrontendFilter\" is no longer required and can be removed. Filters are configured using play's default filter system: https://www.playframework.com/documentation/2.7.x/Filters#Default-Filters")
 
-  override val filters: Seq[EssentialFilter] =
-    enabledFilters.filters ++
-    whenEnabled("bootstrap.filters.allowlist.enabled", allowlistFilter.loadConfig) ++
-    whenEnabled("bootstrap.filters.sessionId.enabled", sessionIdFilter)
-
-  private def whenEnabled(key: String, filter: => EssentialFilter): Seq[EssentialFilter] = {
-    logger.warn(s"""config key $key is deprecated, please configure this filter via play.filters.enabled instead""")
-    if (configuration.get[Boolean](key)) Seq(filter)
-    else Seq.empty
+  override val filters: Seq[EssentialFilter] = {
+    val filters =
+      removeWhenDisabled[SecurityHeadersFilter]("security.headers.filter.enabled")(
+        removeWhenDisabled[CSRFFilter]("bootstrap.filters.csrf.enabled")(
+          addWhenEnabled("bootstrap.filters.allowlist.enabled", allowlistFilter.loadConfig)(
+            addWhenEnabled("bootstrap.filters.sessionId.enabled", sessionIdFilter)(
+              enabledFilters.filters
+            )
+          )
+        )
+      )
+    logger.info(s"EnabledFilters has been amended to ${filters.map(_.getClass.getName).mkString("\n  ", "\n  ", "\n")}")
+    filters
   }
+
+  private def addWhenEnabled[T](key: String, filter: => EssentialFilter)(filters: Seq[EssentialFilter]): Seq[EssentialFilter] =
+    if (configuration.get[Boolean](key))
+      filters :+ filter
+    else
+      filters
+
+  private def removeWhenDisabled[T : ClassTag](key: String)(filters: Seq[EssentialFilter]): Seq[EssentialFilter] =
+    if (!configuration.get[Boolean](key))
+      filters.filter {
+        case f: T => false
+        case f    => true
+      }
+    else
+      filters
 }
