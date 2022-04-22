520c53feee78a06e59329c869a4b30de136dabb8
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2022-04-22 15:10:51 +0100

    BDOG-1807: mitigation

---
+++ bootstrap-common/src/main/scala/uk/gov/hmrc/play/bootstrap/data/UrlEncodedAndMultipartFormBinding.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.play.bootstrap.data
+
+import play.api.data.FormBinding
+import play.api.http.HttpVerbs
+import play.api.mvc.{AnyContent, MultipartFormData, Request}
+
+// Based on play.api.data.DefaultFormBinding - removing the ability to bind forms from JSON
+class UrlEncodedAndMultipartFormBinding extends FormBinding {
+  override def apply(request: play.api.mvc.Request[_]): Map[String, Seq[String]] = {
+    import play.api.mvc.MultipartFormData
+    val unwrap = request.body match {
+      case body: play.api.mvc.AnyContent =>
+        body.asFormUrlEncoded.orElse(body.asMultipartFormData).getOrElse(body)
+      case body => body
+    }
+    val data: Map[String, Seq[String]] = unwrap match {
+      case body: Map[_, _]                   => body.asInstanceOf[Map[String, Seq[String]]]
+      case body: MultipartFormData[_]        => multipartFormParse(body)
+      case Right(body: MultipartFormData[_]) => multipartFormParse(body)
+      case _                                 => Map.empty
+    }
+    (data ++ QueryStringMapper(request)).toMap
+  }
+  private def multipartFormParse(body: MultipartFormData[_]) = body.asFormUrlEncoded
+}
---
+++ bootstrap-common/src/test/scala/uk/gov/hmrc/play/bootstrap/data/UrlEncodedAndMultipartFormBindingSpec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.play.bootstrap.data
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import play.api.libs.json.Json
+import play.api.test.FakeRequest
+import play.mvc.Http.HeaderNames.CONTENT_TYPE
+import uk.gov.hmrc.play.bootstrap.data.MultipartGenerator.aMultipartFileWithParams
+
+class UrlEncodedAndMultipartFormBindingSpec extends AnyWordSpecLike with Matchers {
+
+  val fixture = new UrlEncodedAndMultipartFormBinding()
+
+  "url encoded only form binding" should {
+    "work with application/x-www-form-urlencoded" in {
+      val request = FakeRequest("POST", "/test")
+        .withHeaders(CONTENT_TYPE -> "application/x-www-form-urlencoded")
+        .withFormUrlEncodedBody("test" -> "test")
+
+      val result = fixture.apply(request)
+      result should contain("test" -> Seq("test"))
+    }
+
+    "not work with application/json" in {
+      val request = FakeRequest("POST", "/test")
+        .withHeaders(CONTENT_TYPE -> "application/json")
+        .withJsonBody(Json.parse("""{"test": "test"}"""))
+
+      val result = fixture.apply(request)
+      result shouldBe empty
+    }
+
+    "work with multipart/form-data" in {
+      val request = FakeRequest("POST", "/test")
+        .withHeaders(CONTENT_TYPE -> "multipart/form-data")
+        .withMultipartFormDataBody(aMultipartFileWithParams("test" -> Seq("test")))
+
+      val result = fixture.apply(request)
+      result should contain("test" -> Seq("test"))
+    }
+  }
+
+}
