589ff2ef15374c18c031b2bf3a0adcb60e91f917
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2024-05-28 11:05:58 +0100

    BDOG-3112 Remove deprecated classes

--- bootstrap-common-play-30/src/main/scala/uk/gov/hmrc/play/bootstrap/package.scala
+++ bootstrap-common-play-30/src/main/scala/uk/gov/hmrc/play/bootstrap/package.scala
@@ -16,77 +16,7 @@
 
 package uk.gov.hmrc.play
 
-import javax.inject.{Inject, Named, Provider, Singleton}
-import uk.gov.hmrc.play.audit.http.config.AuditingConfig
-
-import scala.concurrent.{ExecutionContext, Future}
-
-package bootstrap {
-
-  package object binders {
-
-    @deprecated("Use uk.gov.hmrc.play.bootstrap.binders.AbsoluteWithHostnameFromAllowlist instead", "4.0.0")
-    object AbsoluteWithHostnameFromWhitelist {
-
-      def apply(allowedHosts: String*): RedirectUrlPolicy[RedirectUrlPolicy.Id] =
-        apply(allowedHosts.toSet)
-
-      def apply(allowedHosts: Set[String]): RedirectUrlPolicy[RedirectUrlPolicy.Id] =
-        AbsoluteWithHostnameFromAllowlist.apply(allowedHosts)
-
-      def apply(allowedHostsFn: => Future[Set[String]])(implicit ec: ExecutionContext): RedirectUrlPolicy[Future] =
-        AbsoluteWithHostnameFromAllowlist.apply(allowedHostsFn)
-    }
-  }
-
-  @deprecated("Use uk.gov.hmrc.play.audit.AuditModule instead", "5.14.0")
-  class AuditModule extends uk.gov.hmrc.play.audit.AuditModule
-
-  package audit {
-    @deprecated("Use uk.gov.hmrc.play.audit.DefaultAuditChannel instead", "5.14.0")
-    @Singleton
-    class DefaultAuditChannel @Inject()(
-      auditingConfig   : AuditingConfig,
-      materializer     : org.apache.pekko.stream.Materializer,
-      lifecycle        : play.api.inject.ApplicationLifecycle,
-      datastreamMetrics: uk.gov.hmrc.play.audit.http.connector.DatastreamMetrics
-    ) extends uk.gov.hmrc.play.audit.DefaultAuditChannel(
-      auditingConfig,
-      materializer,
-      lifecycle,
-      datastreamMetrics
-    )
-
-    @deprecated("Use uk.gov.hmrc.play.audit.DefaultAuditChannel instead", "5.14.0")
-    class DefaultAuditConnector @Inject()(
-      auditingConfig   : AuditingConfig,
-      auditChannel     : uk.gov.hmrc.play.audit.http.connector.AuditChannel,
-      lifecycle        : play.api.inject.ApplicationLifecycle,
-      datastreamMetrics: uk.gov.hmrc.play.audit.http.connector.DatastreamMetrics
-    ) extends uk.gov.hmrc.play.audit.DefaultAuditConnector(
-      auditingConfig,
-      auditChannel,
-      lifecycle,
-      datastreamMetrics
-    )
-  }
-
-  package config {
-    @deprecated("Use uk.gov.hmrc.play.audit.http.config.AuditingConfig.fromConfig instead", "5.14.0")
-    class AuditingConfigProvider @Inject()(
-      configuration: play.api.Configuration,
-      @Named("appName") appName: String
-    ) extends Provider[AuditingConfig] {
-      override def get(): AuditingConfig =
-        AuditingConfig.fromConfig(configuration)
-    }
-  }
-}
-
 package object bootstrap {
-  @annotation.nowarn("msg=deprecated")
   val deprecatedClasses: Map[String, String] =
-    Map(
-      classOf[uk.gov.hmrc.play.bootstrap.AuditModule].getName -> classOf[uk.gov.hmrc.play.audit.AuditModule].getName,
-    )
+    Map.empty
 }
