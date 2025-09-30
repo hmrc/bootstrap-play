4d253facb4153baa2e809470fc4ac0dacc822a37
MoDweik <180322631+MoDweik@users.noreply.github.com>
2025-09-30 15:06:29 +0100

    BDOG-3538: Remove support for Play 2.9

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object LibDependencies {
-  private val httpVerbsVersion  = "15.5.0"
+  private val httpVerbsVersion  = "15.6.0"
   private val akkaVersion       = "2.6.21"
   private val pekkoVersion      = "1.0.3"
   private val dropwizardVersion = "4.2.32"
@@ -14,9 +14,9 @@ object LibDependencies {
       "io.dropwizard.metrics"   %  "metrics-jvm"                % dropwizardVersion,
       "io.dropwizard.metrics"   %  "metrics-logback"            % dropwizardVersion,
       "uk.gov.hmrc"             %% s"auth-client-$playSuffix"   % "8.6.0",
-      "uk.gov.hmrc"             %% "crypto"                     % "8.3.0",
+      "uk.gov.hmrc"             %% "crypto"                     % "8.4.0",
       "uk.gov.hmrc"             %% s"http-verbs-$playSuffix"    % httpVerbsVersion,
-      "uk.gov.hmrc"             %% s"play-auditing-$playSuffix" % "9.6.0",
+      "uk.gov.hmrc"             %% s"play-auditing-$playSuffix" % "9.7.0",
       // the following are not used by bootstrap - but transitively added for clients
       playOrg(playSuffix)       %% "play-filters-helpers"       % playVersion(playSuffix),
       "uk.gov.hmrc"             %% "logback-json-logger"        % "5.6.0",
@@ -86,19 +86,16 @@ object LibDependencies {
 
   private def playVersion(playSuffix: String) =
     playSuffix match {
-      case "play-29" => "2.9.7"
-      case "play-30" => "3.0.8"
+      case "play-30" => "3.0.9"
     }
 
   private def playOrg(playSuffix: String): String =
     playSuffix match {
-      case "play-29" => "com.typesafe.play"
       case "play-30" => "org.playframework"
     }
 
   private def scalaTestPlusPlayVersion(playSuffix: String): String =
     playSuffix match {
-      case "play-29" => "6.0.1"
       case "play-30" => "7.0.1"
     }
 }
